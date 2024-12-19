3ff7e5097edddbb6419855a6dbfe998ea6c29cf1
Dmitry Mordanov <mordanov314@gmail.com>
2024-12-19 15:29:26 +0300

    in process5

--- make.sh
+++ make.sh
@@ -5,7 +5,9 @@ pwd
 git clone https://github.com/Zep314/bbackup.git
 cd ./bbackup
 pwd
-python3 -m venv .venv
-pip3 install --break-system-packages -r ./requirements.txt
 chmod +x ./make.sh
 chmod +x ./start.sh
+python3 -m venv .venv
+source ./.venv/bin/activate
+pip3 install --break-system-packages -r ./requirements.txt
+deactivate
