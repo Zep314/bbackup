8882c4b05e5bdb2b6520b34f5fcd3f3911d7c8eb
Dmitry Mordanov <mordanov314@gmail.com>
2024-12-19 15:18:40 +0300

    in process3

--- make.sh
+++ make.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd ..
 rm -R ./bbackup
+pwd
 git clone https://github.com/Zep314/bbackup.git
-cd bbackup
+cd ./bbackup
+pwd
 python3 -m venv .venv
 pip install -r ./requirements.txt
 chmod +x ./make.sh
